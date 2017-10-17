5eb0ce6ec8b8f01733f74fc917bfc2e9e218be64
Alexander <alexiceanroid@gmail.com>
2017-10-17 19:10:42 +0300

    1st commit

---
+++ FileSynchronization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+
+namespace FileSynchronization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configReader = new AppSettingsReader();
+            object configLocation = configReader.GetValue("ConfigFile", typeof(string));
+            //Type configType = typeof(configLocation.GetType());
+            Type t = configLocation.GetType();
+        }
+    }
+
+    
+}
