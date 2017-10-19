6a263269b0c3c2e611ed2c0cc902dbb3515a5905
Alexander <alexiceanroid@gmail.com>
2017-10-19 10:35:11 +0300

    2

---
+++ FileSynchronization/FileWatcher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileSynchronization
+{
+    internal class FileWatcher
+    {
+        public FileSystemWatcher Watcher { get; set; }
+
+        private void Watch(string path)
+        {
+            Watcher = new FileSystemWatcher
+            {
+                Path = path,
+                NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                                   | NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                Filter = "*.*"
+            };
+            Watcher.Changed += OnChanged;
+            Watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            uint iterationNum = 0;
+            uint iterationLim = 10;
+            int pauseTime = 1;
+            bool isFileReadyFlag = false;
+
+            while (iterationNum <= iterationLim || isFileReadyFlag)
+            {
+                iterationNum++;
+                isFileReadyFlag = IsFileReady(e.Name);
+                Thread.Sleep(1000 * pauseTime);
+            }
+            Console.WriteLine("file changed!");
+        }
+
+        public static bool IsFileReady(String sFilename)
+        {
+            // If the file can be opened for exclusive access it means that the file
+            // is no longer locked by another process.
+            try
+            {
+                using (FileStream inputStream = File.Open(sFilename, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    return inputStream.Length > 0;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
--- FileSynchronization/Program.cs
+++ FileSynchronization/Program.cs
@@ -7,112 +7,25 @@ using System.IO;
 using System.Configuration;
 using System.Xml;
 using System.Xml.Linq;
+using System.Threading;
 
 namespace FileSynchronization
 {
     class Program
     {
+        private static FileSystemWatcher watcher;
         static void Main(string[] args)
         {
-            
-            GoodSync execInstance = InitializeFolderMappings();
+            // read and initialize source and destination folder mappings:
+            GoodSync execInstance = Init.InitializeFolderMappings();
             
             // initialize source and destination files:
-            InitializeFiles(execInstance);
+            Init.InitializeFiles(execInstance);
 
-        }
-
-        private static void InitializeFiles(GoodSync execInstance)
-        {
-            var folderMappings = execInstance.FolderMappings;
             
 
-            foreach (var pair in folderMappings)
-            {
-                PopulateSourceFiles(execInstance, pair.Key);
-                PopulateDestinationFiles(execInstance, pair.Value);
-            }
-        }
-
-        private static void PopulateDestinationFiles(GoodSync execInstance, string destinationFolder)
-        {
-            GetFileInfos(destinationFolder, execInstance.DestinationFiles);
-        }
-
-        private static void PopulateSourceFiles(GoodSync execInstance, string sourceFolder)
-        {
-            GetFileInfos(sourceFolder, execInstance.SourceFiles);
-        }
-
-        private static void GetFileInfos(string path, HashSet<FileInfo> fileInfos)
-        {
-            
-            if (File.Exists(path))
-            {
-                // This path is a file
-                ProcessFile(path, fileInfos);
-            }
-            else if (Directory.Exists(path))
-            {
-                // This path is a directory
-                ProcessDirectory(path, fileInfos);
-            }
-        }
-
-        // Process all files in the directory passed in, recurse on any directories 
-        // that are found, and process the files they contain.
-        public static void ProcessDirectory(string targetDirectory, HashSet<FileInfo> fileInfos)
-        {
-            // Process the list of files found in the directory.
-            string[] fileEntries = Directory.GetFiles(targetDirectory);
-            foreach (string fileName in fileEntries)
-                ProcessFile(fileName, fileInfos);
-
-            // Recurse into subdirectories of this directory.
-            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
-            foreach (string subdirectory in subdirectoryEntries)
-                ProcessDirectory(subdirectory, fileInfos);
         }
 
         
-        public static void ProcessFile(string path, HashSet<FileInfo> fileInfos)
-        {
-            FileInfo file = new FileInfo(path);
-            fileInfos.Add(file);
-        }
-
-
-        private static GoodSync InitializeFolderMappings()
-        {
-            GoodSync execInstance = new GoodSync();
-            try
-            {
-                var configReader = new AppSettingsReader();
-                string configLocation = (string) configReader.GetValue("ConfigFile", typeof(string));
-
-
-                XElement root = XElement.Load(configLocation);
-
-
-                IEnumerable<XElement> mappingCollection = from m in root.Element("mappings").Elements("mapping")
-                    select m;
-
-                foreach (XElement el in mappingCollection)
-                {
-                    string sourceFolder = el.Element("SourceFolder").Value;
-                    string destFolder = el.Element("DestinationFolder").Value;
-                    execInstance.folderMappings.Add(sourceFolder, destFolder);
-                }
-            }
-            catch (Exception e)
-            {
-                execInstance.ResultStatus = ResultStatusEnum.Error;
-                execInstance.ResultInfo = e.Message;
-            }
-            
-            return execInstance;
-            
-        }
     }
-   
 }
